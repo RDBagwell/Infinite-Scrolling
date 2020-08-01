9a88c215489b59e93ba985dc6fa87819524d9749
Robert Bagwell <erbagwell@yahoo.com>
2020-08-01 09:26:09 -0600

    Optimize code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-const apiKey = "p6HhWJToxq6VoGnN-xRGkH_2zIR0FL7MNBR56Oj3YTo";
 let getImageCount = 3;
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${getImageCount}`;
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader');
-
-
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 let photosArray = [];
+
+const apiKey = "p6HhWJToxq6VoGnN-xRGkH_2zIR0FL7MNBR56Oj3YTo";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${getImageCount}`;
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
 
 function imageLoader(){
     imagesLoaded++;
@@ -48,7 +47,6 @@ function displayPhotos(){
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
-
 }
 
 async function getPhotos(){
